390f1a76a63d3c85c7b87295092a21ef73d53460
Todd Best <toddbest2004@gmail.com>
2015-12-10 12:48:21 -0800

    Basic starting structure.

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var db = require('./mongoose')
+var path = require('path')
+
+var app = express()
+
+app.use(express.static(path + '/static'))
+
+var gameCtrl = require("./controllers/game")
+app.use("/game", gameCtrl)
+
+var port = process.env.PORT || 3000
+var serverip = process.env.IP || "localhost"
+
+app.listen(port, serverip)
+console.log('Server running at '+serverip+":"+port)
